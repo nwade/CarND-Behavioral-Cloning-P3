414b87c8e7cae7d3d31baccf1a1942ab6559cbee
Nick Wade <nick@nwade.com>
2017-08-01 20:24:20 -0600

    Add scaffold script and writeup progress

---
+++ playground.py
@@ -0,0 +1,17 @@
+# Get an AWS GPU instance up and running
+# Build a simple model
+# Add normalization and mean centering using a Lambda
+# Augment data by flipping images with inverse values
+# Augment data with recovery laps showing only corrections
+# Build NVIDIA pipeline
+# Consider using other camera images
+# Crop and scale down images
+# Record extra laps
+    # 2-3 good laps with center driving
+    # 1 recovery lap
+    # 1 lap very smooth in curves
+    # ? in opposite direction
+# Can visualize history with pyplot graphs
+# Use generators to load data on demand
+# Record video of 1+ autonomous laps
+
