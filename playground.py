aaa9fb6b9ae5aef1f951ee35e550474825527201
Nick Wade <nick@nwade.com>
2017-08-02 03:09:18 +0000

    Create simple model in playground
    This model is fairly useless other than for testing the process flow

--- playground.py
+++ playground.py
@@ -1,5 +1,39 @@
-# Get an AWS GPU instance up and running
-# Build a simple model
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+  reader = csv.reader(csvfile)
+  for line in reader:
+    lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+  source_path = line[0]
+  filename = source_path.split('/')[-1]
+  current_path = './data/IMG/' + filename
+  image = cv2.imread(current_path)
+  images.append(image)
+  measurement = float(line[3])
+  measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+
+model.save('model.h5')
+
 # Add normalization and mean centering using a Lambda
 # Augment data by flipping images with inverse values
 # Augment data with recovery laps showing only corrections
@@ -14,4 +48,3 @@
 # Can visualize history with pyplot graphs
 # Use generators to load data on demand
 # Record video of 1+ autonomous laps
-
