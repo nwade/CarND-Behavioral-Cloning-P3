5bffe298404087b10765f14586ddd98195b20c97
Nick Wade <nick@nwade.com>
2017-08-02 21:11:01 -0600

    Use LetNet network with added lambda normalization

--- playground.py
+++ playground.py
@@ -1,10 +1,15 @@
 import csv
 import cv2
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
+  next(reader)
   for line in reader:
     lines.append(line)
 
@@ -22,15 +27,20 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
-
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320,3)))
+model.add(Conv2D(6, (5,5), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D((6, (5,5), activation='relu')))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, verbose=1)
 
 model.save('model.h5')
 
