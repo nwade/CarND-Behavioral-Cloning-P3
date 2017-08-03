e48502383fceb2fbed53f13d512a576841fd4a47
Nick Wade <nick@nwade.com>
2017-08-02 23:37:13 -0600

    Use generators to load data

--- playground.py
+++ playground.py
@@ -6,51 +6,66 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import sklearn
 
-images = []
-measurements = []
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+
+    while 1:
+        shuffle(samples)
+
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            images = []
+            angles = []
+            for sample in batch_samples:
+                directory = './data/'
+
+                img_center = cv2.imread(directory + sample[0].lstrip())
+                img_left = cv2.imread(directory + sample[1].lstrip())
+                img_right = cv2.imread(directory + sample[2].lstrip())
+                images.extend([
+                    img_center,
+                    np.fliplr(img_center),
+                    img_left,
+                    np.fliplr(img_left),
+                    img_right,
+                    np.fliplr(img_right)
+                ])
+
+                correction = 0.2
+                angle_center = float(sample[3])
+                angle_left = angle_center + correction      # go right
+                angle_right = angle_center - correction     # go left
+                angles.extend([
+                    angle_center,
+                    -angle_center,
+                    angle_left,
+                    -angle_left,
+                    angle_right,
+                    -angle_right
+                ])
+
+                X_train = np.array(images)
+                y_train = np.array(angles)
+
+                yield sklearn.utils.shuffle(X_train, y_train)
+
+rows = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    next(reader) # Skip column names
+    for line in reader:
+        rows.append(line)
 
-    for row in reader:
-        correction = 0.2
-        steering_center = float(row[3])
-        steering_left = steering_center + correction    # Turn right
-        steering_right = steering_center - correction   # Turn left
+train_samples, validation_samples = train_test_split(rows, test_size=0.2)
 
-        steering_center_flip = -steering_center
-        steering_left_flip = -steering_left
-        steering_right_flip = -steering_right
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
-        directory = './data/'
-
-        img_center = cv2.imread(directory + row[0].lstrip())
-        img_left = cv2.imread(directory + row[1].lstrip())
-        img_right = cv2.imread(directory + row[2].lstrip())
-
-        img_center_flip = np.fliplr(img_center)
-        img_left_flip = np.fliplr(img_left)
-        img_right_flip = np.fliplr(img_right)
-
-        images.extend([
-            img_center,
-            img_center_flip,
-            img_left,
-            img_left_flip,
-            img_right,
-            img_right_flip])
-        measurements.extend([
-            steering_center,
-            steering_center_flip,
-            steering_left,
-            steering_left_flip,
-            steering_right,
-            steering_right_flip])
-
-
-X_train = np.array(images)
-y_train = np.array(measurements)
 
 model = Sequential()
 
@@ -71,13 +86,13 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(
-    X_train,
-    y_train,
-    validation_split=0.2,
-    shuffle=True,
-    epochs=1,
-    verbose=1)
+history_object = model.fit_generator(
+    train_generator,
+    steps_per_epoch = len(train_samples),
+    validation_data = validation_generator,
+    validation_steps = len(validation_samples),
+    epochs = 1,
+    verbose = 1)
 
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
