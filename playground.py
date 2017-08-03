ff574ed853e5c188869ddf983387e039a4843239
Nick Wade <nick@nwade.com>
2017-08-02 22:44:48 -0600

    Use NVIDIA architecture

--- playground.py
+++ playground.py
@@ -55,13 +55,18 @@ model = Sequential()
 
 model.add(Cropping2D(cropping=((60,20), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Conv2D(6, (5,5), activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(6, (5,5), activation='relu'))
-model.add(MaxPooling2D())
+
+model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(35, (5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(48, (5,5), strides=(2,2), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
+
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
@@ -70,7 +75,6 @@ model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, verbos
 model.save('model.h5')
 
 # Augment data with recovery laps showing only corrections
-# Build NVIDIA pipeline
 # Record extra laps
     # 2-3 good laps with center driving
     # 1 recovery lap
